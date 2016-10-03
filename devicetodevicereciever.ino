c7763206663ba36c95e132f2260b8ee41084687a
robmckay <robmckay@users.noreply.github.com>
2016-10-03 18:30:43 +0100

    Add files via upload

---
+++ devicetodevicereciever.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <ThingerWifi.h>
+
+ThingerWifi thing("robmckay123", "d1mini", "d1mini");
+
+void setup() {
+  
+  thing.add_wifi("BTHub5-NQ38", "schoolroadmassive");
+
+   pinMode(5, OUTPUT);
+   pinMode(15, INPUT);
+  thing["blink"] = [](){
+      for(int i=0; i <=255; i++){
+       analogWrite(5,i);
+        delay(1);
+      }
+      delay(300);
+      for(int i=255; i >=0; i--){
+       analogWrite(5,i);
+       delay(2);
+      }
+    };
+
+  
+}
+
+void loop() {
+  thing.handle();
+  if(digitalRead(15) ==1) {
+  
+    thing.call_endpoint("LEDnode");
+    delay(200);   
+  }
+  
+}
---
+++ devicetodevicesender.ino.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <ThingerWifi.h>
+
+ThingerWifi thing("robmckay123", "nodemcu", "nodemcu");
+int state =0;
+void setup() {
+  
+  thing.add_wifi("BTHub5-NQ38", "schoolroadmassive");
+
+   pinMode(15, INPUT);
+   pinMode(5, OUTPUT);
+  thing["blinknode"] = [](){
+
+    digitalWrite(5,1);
+    delay(50);
+    digitalWrite(5,0);
+    };
+}
+
+void loop() {
+  thing.handle();
+  
+  if(digitalRead(15) ==1) {
+  
+    thing.call_endpoint("LED");
+    delay(200);   
+  }
+}
